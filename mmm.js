906ab0721523e2ed76442f726b07b2e6e3dda922
kmaha2644 <kmaha2644@gmail.com>
2026-01-26 10:59:26 +0500

    Create mmm.js

---
+++ mmm.js
@@ -0,0 +1,43 @@
+// let num1=+prompt("enter num1");
+// let num2=+prompt("enter num2");
+// let opp=prompt("enter any opperator");
+
+// switch(opp){
+//     case "+":
+//     document.write(num1+num2);
+//     break;
+//     case "-":
+//     document.write(num1-num2);
+//     break;
+//     case "*":
+//     document.write(num1*num2);
+//     break;
+//     case "/":
+//     document.write(num1/num2);
+//     break;
+//     case "%":
+//     document.write(num1/num2*100);
+//     break;
+//     default:
+//         document.write("your opperator is invalid");
+// }
+
+
+// // let num=1;
+// // document.write(num+"<br>");
+
+// // let num1=2;
+// // document.write(num1+"<br>");
+
+
+// for(i=0;i<100;i++){
+//     document.write(i+"<br>");
+// }
+
+// for(i=1;i<101;i++){
+//     document.write(i+"<br>");
+// }
+
+// for(i=1;i<=100;i++){
+//     document.write(i+"<br>");
+// } 
